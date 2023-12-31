8aa2a5c5c642e3362c9c5a549c2b00c2acbca1c4
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2023-12-31 12:36:17 +0100

    Removed html to js, new function & loop
    Removed data frm html to js file
    Created a function displayForecast
    Created a loop
    Called the function

--- src/index.js
+++ src/index.js
@@ -48,5 +48,29 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+             <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-icon">⛅</div>
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature"><strong>18°C<strong></div>
+                <div class="weather-forecast-temperature">12°C</div>
+              </div>
+            </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let forecastElement = document.querySelector("#forecast");
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
+displayForecast();
