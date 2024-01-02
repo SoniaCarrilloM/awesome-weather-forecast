e3df1e410455332320a08973977ffd2f9b5b59d8
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-01-02 10:46:20 +0100

    Finalized adding forecsat API
    replaced getForecast call to function refreshWeather

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -48,15 +50,16 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-
 function getForecast(city) {
-let apiKey:"b2a5adcct04b33178913oc335f405433";
-let apiUrl: `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-console.log(apiUrl);
-
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
-function displayForecast() {
+
+function displayForecast(response) {
+  console.log(response.data);
+
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
@@ -81,5 +84,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-getForecast("Paris");
-displayForecast();
